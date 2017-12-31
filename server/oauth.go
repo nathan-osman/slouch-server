14133e7f61285418776ebe490dcb4d726c78b233
Nathan Osman <nathan@quickmediasolutions.com>
2017-12-30 17:37:07 -0800

    Implement OAuth return and error page.

--- server/oauth.go
+++ server/oauth.go
@@ -1,19 +1,22 @@
 package server
 
 import (
+	"encoding/json"
+	"errors"
 	"net/http"
 	"net/url"
 
 	"github.com/flosch/pongo2"
 )
 
-// TODO: make redirect_uri a configurable setting
+// TODO: make this a configurable setting
+var redirectUri = "https://slouch.quickmediasolutions.com/oauth/return"
 
 func (s *Server) oauthBegin(w http.ResponseWriter, r *http.Request) {
 	q := url.Values{}
 	q.Set("client_id", s.clientID)
 	q.Set("scope", "chat:write:user")
-	q.Set("redirect_uri", "https://slouch.quickmediasolutions.com/oauth/return")
+	q.Set("redirect_uri", redirectUri)
 	u, err := url.Parse("https://slack.com/oauth/authorize")
 	if err != nil {
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
@@ -27,7 +30,48 @@ func (s *Server) oauthBegin(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *Server) oauthReturn(w http.ResponseWriter, r *http.Request) {
-	s.render(w, r, "oauth/return.html", pongo2.Context{
-		"title": "Return",
-	})
+	accessToken, err := func() (string, error) {
+		code := r.URL.Query().Get("code")
+		if len(code) == 0 {
+			return "", errors.New("empty verification code")
+		}
+		q := url.Values{}
+		q.Set("client_id", s.clientID)
+		q.Set("client_secret", s.clientSecret)
+		q.Set("code", code)
+		q.Set("redirect_uri", redirectUri)
+		u, err := url.Parse("https://slack.com/api/oauth.access")
+		if err != nil {
+			return "", err
+		}
+		u.RawQuery = q.Encode()
+		resp, err := http.Get(u.String())
+		if err != nil {
+			return "", err
+		}
+		defer resp.Body.Close()
+		var accessResponse struct {
+			OK          bool   `json:"ok"`
+			Error       string `json:"error"`
+			AccessToken string `json:"access_token"`
+		}
+		if err := json.NewDecoder(resp.Body).Decode(&accessResponse); err != nil {
+			return "", err
+		}
+		if !accessResponse.OK {
+			return "", errors.New(accessResponse.Error)
+		}
+		return accessResponse.AccessToken, nil
+	}()
+	if err != nil {
+		s.render(w, r, "error.html", pongo2.Context{
+			"title": "Error",
+			"error": err.Error(),
+		})
+	} else {
+		s.render(w, r, "oauth/return.html", pongo2.Context{
+			"title":        "Return",
+			"access_token": accessToken,
+		})
+	}
 }
